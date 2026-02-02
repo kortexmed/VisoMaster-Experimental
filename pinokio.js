f64fbbd0daa52e885d64679d89b01cddad339234
kortexmed <75435615+kortexmed@users.noreply.github.com>
2026-02-02 18:53:50 +0100

    Update pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -3,26 +3,23 @@ module.exports = {
   "title": "VisoMaster Experimental",
   "description": "Logiciel puissant et facile à utiliser pour l'échange et le montage vidéo de visages.",
   "icon": "icon.png",
-  "menu": async (kernel, info) => {
-    let installed = info.exists("app/env")
-    if (installed) {
-      return [{
-        default: true,
-        text: "start",
-        href: "start.json",
-      }, {
-        text: "install",
-        href: "install.json"
-      }]
-    } else {
-      return [{
-        text: "start",
-        href: "start.json",
-      }, {
-        default: true,
-        text: "install",
-        href: "install.json"
-      }]
+  "menu": [
+    {
+      "when": "{{!fs.existsSync('app')}}",
+      "text": "install",
+      "href": "install.json",
+      "default": true
+    },
+    {
+      "when": "{{fs.existsSync('app')}}",
+      "text": "start",
+      "href": "start.json",
+      "default": true
+    },
+    {
+      "text": "update",
+      "href": "update.json",
+      "when": "{{fs.existsSync('app')}}"
     }
-  }
+  ]
 }
